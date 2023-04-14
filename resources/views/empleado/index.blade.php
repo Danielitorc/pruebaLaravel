f5e53ad06f569c0bb93ff9ff95e219b24c7fb343
Danielitorc <drangel664@gmail.com>
2023-04-14 17:51:31 -0600

    Presentacion de datos terminada desde la BD

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,28 @@
-//Aqui se mostrara la lista de empleados 
+//Aqui se mostrara la lista de empleados
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Edad</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+        @foreach($empleados as $empleado)
+            <tr>
+                <td>{{ $empleado->id }}</td>
+                <td>{{ $empleado->Nombre }}</td>
+                <td>{{ $empleado->apellidoPaterno }}</td>
+                <td>{{ $empleado->apellidoMaterno }}</td>
+                <td>{{ $empleado->Edad }}</td>
+                <td>{{ $empleado->Correo }}</td>
+                <td>Editar | Borrar</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table> 
